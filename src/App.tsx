42615e3eb1bfae486c214ed32fd46c5f0a1139f9
nonsense00027 <caneteneilfred@gmail.com>
2023-09-21 09:59:15 +0800

    fix tooltip styling

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       </main>
       <div className="fixed bottom-8 right-8 lg:bottom-20 lg:right-20">
         <div className="group relative w-16 h-16 bg-blue-600 border-white border rounded-full grid place-content-center shadow-md cursor-pointer hover:bg-blue-700 hover:scale-110 has-transition">
-          <p className="absolute hidden w-36 bottom-20 left-[50%] -translate-x-[50%] text-xs bg-white border rounded-full px-4 py-1 opacity-0 group-hover:opacity-100 lg:block has-transition">
+          <p className="absolute hidden whitespace-nowrap bottom-20 left-[50%] -translate-x-[50%] text-xs bg-white border rounded-full px-4 py-1 opacity-0 group-hover:opacity-100 lg:block has-transition">
             Create Appointment
           </p>
           <PlusIcon />
